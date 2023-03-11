1655ddfc81fbd7a16f3aff5a9e45f81db6b04a1e
tranduc2501 <65280079+tranduc2501@users.noreply.github.com>
2023-03-11 13:18:44 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print('hello')
+tienhienco = 200
+lai1nam = 1.08
+#In ra giá trị 
+print(tienhienco)
+print(lai1nam)
+#Tiền lãi 10 năm
+print(lai10nam)
+#In ra tích 
+print(tienhienco*lai1nam)
+#In ra tổng 
+tong=tienhieco*lai1nam
+print("Tổng của tienhienco và lai1nam là: " , tong)
+#Tính tích tiền hiện có sau 10 năm lãi và in ra màn hình
+KQ = tienhienco*lai10nam
+print("Số tiền của bạn sau 10 năm là:",KQ)
+
